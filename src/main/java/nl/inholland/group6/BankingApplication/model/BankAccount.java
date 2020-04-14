cfde0fd9cac893d7c4e61ef6a806ddb25462403a
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-04-14 10:08:04 +0200

    Starting to make models

--- src/main/java/nl/inholland/group6/BankingApplication/model/BankAccount.java
+++ src/main/java/nl/inholland/group6/BankingApplication/model/BankAccount.java
@@ -2,4 +2,8 @@ package nl.inholland.group6.BankingApplication.model;
 
 public class BankAccount {
 
+    long id;
+    int pin;
+    //user
+        //make savings and current to inherit from this class.
 }
