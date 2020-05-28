b3e306618c45da87d43d882b3dfd8f9809d20028
munatsimike <42965915+munatsimike@users.noreply.github.com>
2020-05-29 00:10:21 +0200

    Accounts

--- src/main/java/io/swagger/repository/AccountRepository.java
+++ src/main/java/io/swagger/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package io.swagger.repository;
 
 import io.swagger.model.Account;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account,Integer> {
 }
