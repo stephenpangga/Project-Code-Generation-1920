a6e11e3a9f8d79b9d443e6e7ba7608be3f3c73ed
munatsimike <42965915+munatsimike@users.noreply.github.com>
2020-06-06 14:46:26 +0200

    fixing account model - removing unnecessary inheritance with michael

--- src/test/java/io/swagger/model/AccountTest.java
+++ src/test/java/io/swagger/model/AccountTest.java
@@ -5,19 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AccountTest {
-Account acc = null;
-@BeforeEach
-public void Start(){
-    acc = new Account();
-}
+    class AccountTest {
+    Account acc = null;
+    @BeforeEach
+    public void Setup(){
+        acc = new Account();
+    }
     @Test
     public void WhenICreatNewAccShouldNotBeNull(){
         assertNotNull(acc);
     }
     @Test
-    public void CurrencyShldBeEuro(){
-        assertEquals("Euro", acc.getCurrency());
+    public void CurrencyIsEuro(){
+        assertTrue(acc.getCurrency() == "Euro");
+    }
+    @Test
+    public void IBANLegnthIs18(){
+        assertTrue(acc.getIban().length() == 18);
     }
 
 }
