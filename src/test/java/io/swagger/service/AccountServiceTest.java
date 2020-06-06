11e1ee7561dbba1da1d03f039b9a97157c42c071
munatsimike <42965915+munatsimike@users.noreply.github.com>
2020-06-06 14:05:31 +0200

    fixing account model - removing unnecessary inheritance with michael

---
+++ src/test/java/io/swagger/service/AccountServiceTest.java
@@ -0,0 +1,43 @@
+package io.swagger.service;
+
+import io.swagger.model.Account;
+import io.swagger.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountServiceTest {
+
+    @Autowired
+    private MockMvc mvc;
+   @Autowired
+   AccountService service;
+    @MockBean
+    private AccountRepository accountRepository;
+    private Account account;
+
+    @BeforeEach
+    public void Setup(){
+        account = new Account(2, Account.AccountTypeEnum.SAVINGS);
+    }
+    @Test
+    public void CallingAllAccountsShouldReturnOK() throws Exception {
+        given( accountRepository.findAll())
+                .willReturn(Arrays.asList(account));
+        service.perform(service.GetAllAccounts())
+                .andExpect(status().isOk());
+    }
+
+}
