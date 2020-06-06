0b4324792c0712a53f5308506e6f7c7d66984ed1
FrancesMikaela <frances.mikalea03@gmail.com>
2020-06-06 14:24:33 +0200

    Fixing register user

--- src/main/java/io/swagger/api/UsersApiController.java
+++ src/main/java/io/swagger/api/UsersApiController.java
@@ -108,7 +108,7 @@ public class UsersApiController implements UsersApi {
         }
 
         //return new ResponseEntity<User>(HttpStatus.NOT_IMPLEMENTED);
-        return new ResponseEntity<User>(userService.registerUser("test123@gmai.com","test","test","123", User.AccessLevelEnum.CUSTOMER), HttpStatus.OK);
+        return new ResponseEntity<User>(userService.registerUser("test02@gmail.com","test02","test","02", User.AccessLevelEnum.CUSTOMER), HttpStatus.OK);
     }
 
     public ResponseEntity<Void> updateUser(@ApiParam(value = "User id to get from the database",required=true) @PathVariable("userId") Integer userId
