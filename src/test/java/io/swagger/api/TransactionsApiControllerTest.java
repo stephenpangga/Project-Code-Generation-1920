e67418eebf575e9adf658974f95d50458673fdbc
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-06-07 23:27:08 +0200

    fixing all the merging errors

--- src/test/java/io/swagger/api/TransactionsApiControllerTest.java
+++ src/test/java/io/swagger/api/TransactionsApiControllerTest.java
@@ -1,7 +1,108 @@
 package io.swagger.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.Account;
+import io.swagger.model.Transaction;
+import io.swagger.model.User;
+import io.swagger.service.TransactionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.threeten.bp.LocalDateTime;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+//@WebMvcTest(Controller.class)
 class TransactionsApiControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    //private TransactionsApiController transactionsApiController;
+    private TransactionService transactionService;
+
+    //@MockBean
+    //private TransactionRepository transactionRepository;
+
+    private Transaction transaction;
+
+    @BeforeEach
+    public void setup()
+    {
+        User employee = new User("inholland@gmail.com",
+                "inhollandbank",
+                "Bank",
+                "bank",
+                User.AccessLevelEnum.EMPLOYEE);
+        User stephen = new User("stephen@gmail.com",
+                "password",
+                "Stephen",
+                "Pangga",
+                User.AccessLevelEnum.EMPLOYEE);
+        Account bankAccount = new Account("NL01INHO0000000001",100.0, employee, Account.AccountTypeEnum.CURRENT);
+        Account stephenAccount = new Account("NL01INHO0000000019",100.0, stephen, Account.AccountTypeEnum.CURRENT);
+
+        transaction = new Transaction(bankAccount,
+                stephenAccount,
+                100.0,
+                Transaction.TransactionTypeEnum.TRANSFER,
+                employee,
+                LocalDateTime.now());
+    }
+
+    //(expected = NullPointerException.class)
+    //@WithMockUser(roles = {"Employee", "Customer"})
+    @Test
+    public void gettingAllTransactionShouldReturnOK() throws Exception {
+
+        List<Transaction> transactionList = Arrays.asList(transaction);
+
+        given(transactionService.getAllTransactions()).willReturn(transactionList);
+
+        this.mvc.perform(get("/transactions"))
+                .andExpect(status().isOk()).andExpect(content().contentType("application/json"));
+    }
+
+    @Test
+    public void test() throws Exception {
+        List<Transaction> transactionList = Arrays.asList(transaction);
+
+        given(transactionService.getAllTransactions()).willReturn(transactionList);
+
+        this.mvc.perform(get("/transactions"))
+                .andExpect(status().isOk()).andExpect(content().contentType("application/json"));
+
+    }
+    @Test
+    public void createTransactionShouldReturn201Created() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        this.mvc
+                .perform(post("/transactions")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(transaction)))
+                .andExpect(status().isCreated());
+    }
+
 }
+
