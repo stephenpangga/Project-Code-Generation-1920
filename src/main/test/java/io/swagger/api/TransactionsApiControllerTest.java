cd80e51a90c965c9af6b0b5863e6f8f96f3c2c90
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-06-07 21:53:50 +0200

    adding some more test units

--- src/main/test/java/io/swagger/api/TransactionsApiControllerTest.java
+++ src/main/test/java/io/swagger/api/TransactionsApiControllerTest.java
@@ -10,10 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.threeten.bp.LocalDateTime;
@@ -27,10 +27,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-@WebMvcTest(TransactionsApiController.class)
+//@WebMvcTest(Controller.class)
 class TransactionsApiControllerTest {
 
     @Autowired
