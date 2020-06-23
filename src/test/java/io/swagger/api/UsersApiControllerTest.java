caacdf62103537ccff66421a004f081574a75d67
FrancesMikaela <frances.mikalea03@gmail.com>
2020-06-23 15:49:53 +0200

    working on user testing

--- src/test/java/io/swagger/api/UsersApiControllerTest.java
+++ src/test/java/io/swagger/api/UsersApiControllerTest.java
@@ -1,18 +1,26 @@
 package io.swagger.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.Transaction;
 import io.swagger.model.User;
 import io.swagger.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -28,15 +36,28 @@ class UsersApiControllerTest {
     @MockBean
     private UserService userService;
 
+    @BeforeEach
     public void setUp(){
         user = new User("jjk@gmail.com","jjk97","jeon","jungkook", User.AccessLevelEnum.CUSTOMER);
     }
 
     @Test
     public void callingAllUserShouldReturnOK() throws Exception {
-        given(userService.getAllUser()).willReturn(Arrays.asList(user));
+        List<User> userList = Arrays.asList(user);
+        given(userService.getAllUser()).willReturn(userList);
 
-        this.mvc.perform(get("/users")).andExpect(status().isOk());
+        this.mvc.perform(get("/users")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void createUserShouldReturn201Created() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        this.mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isCreated());
     }
 
 
