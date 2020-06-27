eedb3ec292b73f4e875e1c6c7482b8d0da0d9965
Galaxy <thomaswillyam@live.nl>
2020-06-27 14:40:05 +0200

    Add authorization to Account endpoints

--- src/main/java/io/swagger/api/AccountsApiController.java
+++ src/main/java/io/swagger/api/AccountsApiController.java
@@ -3,7 +3,9 @@ package io.swagger.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
 import io.swagger.model.Account;
+import io.swagger.model.User;
 import io.swagger.service.AccountService;
+import io.swagger.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,9 @@ public class AccountsApiController implements AccountsApi {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private LoginService loginService;
+
     @org.springframework.beans.factory.annotation.Autowired
     public AccountsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
@@ -38,72 +43,90 @@ public class AccountsApiController implements AccountsApi {
 
     public ResponseEntity<Void> accountsDelete(@ApiParam(value = "Account IBAN to delete",required=true) @PathVariable("IBAN") String IBAN
 )   {
-        accountService.DeleteAccount(IBAN);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.EMPLOYEE)) {
+            accountService.DeleteAccount(IBAN);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
     }
 
     public ResponseEntity<List<Account>> accountsGet(@ApiParam(value = "The number of items to skip before starting to collect the result set") @Valid @RequestParam(value = "offset", required = false) Integer offset
 ,@ApiParam(value = "The numbers of items to return") @Valid @RequestParam(value = "limit", required = false) Integer limit
 ,@ApiParam(value = "The balance of the account") @Valid @RequestParam(value = "balance", required = false) Integer balance
 ) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Account>>(objectMapper.readValue("[ {\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}, {\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Account>>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
+            String accept = request.getHeader("Accept");
+            if (accept != null && accept.contains("application/json")) {
+                try {
+                    return new ResponseEntity<List<Account>>(objectMapper.readValue("[ {\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}, {\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+                } catch (IOException e) {
+                    log.error("Couldn't serialize response for content type application/json", e);
+                    return new ResponseEntity<List<Account>>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
-        }
 
-        return new ResponseEntity<List<Account>>(accountService.GetAllAccounts(),HttpStatus.OK);
+            return new ResponseEntity<List<Account>>(accountService.GetAllAccounts(),HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
     }
 
     public ResponseEntity<Account> accountsIBANGet(@ApiParam(value = "Account IBAN to find",required=true) @PathVariable("IBAN") String IBAN
 ) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}", Account.class), HttpStatus.OK);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
+            String accept = request.getHeader("Accept");
+            if (accept != null && accept.contains("application/json")) {
+                try {
+                    return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}", Account.class), HttpStatus.OK);
+                } catch (IOException e) {
+                    log.error("Couldn't serialize response for content type application/json", e);
+                    return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
-        }
 
-        return new ResponseEntity<Account>(accountService.GetAccount(IBAN),HttpStatus.NOT_IMPLEMENTED);
+            return new ResponseEntity<Account>(accountService.GetAccount(IBAN),HttpStatus.NOT_IMPLEMENTED);
+        } else {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
     }
 
     public ResponseEntity<Account> accountsIBANPut(@ApiParam(value = "Account IBAN to find",required=true) @PathVariable("IBAN") String IBAN
 ,@ApiParam(value = ""  )  @Valid @RequestBody Account body
 ) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
+            String accept = request.getHeader("Accept");
+            if (accept != null && accept.contains("application/json")) {
+                try {
+                    return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"accountType\" : \"current\",\n  \"authorId\" : 1\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
+                } catch (IOException e) {
+                    log.error("Couldn't serialize response for content type application/json", e);
+                    return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
-        }
 
-        return new ResponseEntity<Account>(accountService.UpdateAccount(body,IBAN),HttpStatus.OK);
+            return new ResponseEntity<Account>(accountService.UpdateAccount(body,IBAN),HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
     }
 
     public ResponseEntity<Account> accountsPost(@ApiParam(value = "creates a new account for a existing user" ,required=true )  @Valid @RequestBody Account body
 ) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"balance\" : 0,\n  \"iban\" : \"NL23INHO2298608059\"\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
+            String accept = request.getHeader("Accept");
+            if (accept != null && accept.contains("application/json")) {
+                try {
+                    return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"balance\" : 0,\n  \"iban\" : \"NL23INHO2298608059\"\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
+                } catch (IOException e) {
+                    log.error("Couldn't serialize response for content type application/json", e);
+                    return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
+
+            return new ResponseEntity<Account>(accountService.CreateAccount(body),HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
         }
-
-        return new ResponseEntity<Account>(accountService.CreateAccount(body),HttpStatus.CREATED);
     }
-
-
 }
