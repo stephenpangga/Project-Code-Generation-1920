e4932dc77e6c3b314747ca9c9925987eb9508e6d
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-06-28 15:19:22 +0200

    adding cross allow origin code

---
+++ src/main/java/io/swagger/configuration/CrossConfiguration.java
@@ -0,0 +1,19 @@
+package io.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class CrossConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
