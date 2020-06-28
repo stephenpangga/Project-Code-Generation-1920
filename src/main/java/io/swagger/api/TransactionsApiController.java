cfc3e7a950c5efa9492d65c7484b8e38a47f3704
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-06-28 16:49:40 +0200

    transaction controller - removing some generated code

--- src/main/java/io/swagger/api/TransactionsApiController.java
+++ src/main/java/io/swagger/api/TransactionsApiController.java
@@ -49,24 +49,6 @@ public class TransactionsApiController implements TransactionsApi {
     {
 
         if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
-/*
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Transaction>>(objectMapper.readValue("[ {\n  \"transactionType\" : \"transaction\",\n  \"transaction_id\" : 488558,\n  \"amount\" : 500.73,\n  \"userPerforming\" : 1,\n  \"sender\" : \"NL01INHO0000000001\",\n  \"recipient\" : \"NL53INHO0858545222\"\n}, {\n  \"transactionType\" : \"transaction\",\n  \"transaction_id\" : 488558,\n  \"amount\" : 500.73,\n  \"userPerforming\" : 1,\n  \"sender\" : \"NL01INHO0000000001\",\n  \"recipient\" : \"NL53INHO0858545222\"\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Transaction>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        */
-
-        /*
-        if(userPerforming != null)
-        {
-            //userService.getById(userPerforming);
-        }
-        */
 
             List<Transaction> transactions = null;
 
@@ -76,9 +58,6 @@ public class TransactionsApiController implements TransactionsApi {
             }catch (Exception e) {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
             }
-
-            //System.out.println(iban);
-            //return new ResponseEntity<List<Transaction>>(transactionService.getAllTransactions(),HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.FORBIDDEN);
         }
@@ -87,29 +66,6 @@ public class TransactionsApiController implements TransactionsApi {
     public ResponseEntity<Transaction> getTransactionsById(@ApiParam(value = "",required=true) @PathVariable("transactionId") Integer transactionId
 ) {
         if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
-/*
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Transaction>(objectMapper.readValue("{\n  \"transactionType\" : \"transaction\",\n  \"transaction_id\" : 488558,\n  \"amount\" : 500.73,\n  \"userPerforming\" : 1,\n  \"sender\" : \"NL01INHO0000000001\",\n  \"recipient\" : \"NL53INHO0858545222\"\n}", Transaction.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Transaction>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        */
-            //return new ResponseEntity<Transaction>(HttpStatus.NOT_IMPLEMENTED);
-        /*
-        ResponseEntity<Transaction> x = new ResponseEntity<Transaction>(
-                new Transaction("NL01INHO1",
-                        "NL02INHO2",
-                        500.73,
-                        Transaction.TransactionTypeEnum.TRANSFER,
-                        1,
-                        LocalDateTime.now()),HttpStatus.OK);
-        return x;
-        */
-
             return new ResponseEntity<Transaction>(transactionService.getSpecificTransaction(transactionId),HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.FORBIDDEN);
@@ -119,21 +75,8 @@ public class TransactionsApiController implements TransactionsApi {
     public ResponseEntity transfer(@ApiParam(value = ""  )  @Valid @RequestBody Transaction body)
     {
         if (loginService.isUserAuthorized(request.getHeader("Authorization"), User.AccessLevelEnum.CUSTOMER)) {
-/*
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Transaction>(objectMapper.readValue("{\n  \"transactionType\" : \"transaction\",\n  \"transaction_id\" : 488558,\n  \"amount\" : 500.73,\n  \"userPerforming\" : 1,\n  \"sender\" : \"NL01INHO0000000001\",\n  \"recipient\" : \"NL53INHO0858545222\"\n}", Transaction.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Transaction>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        */
             //set the date here.
             body.setDatetime(LocalDateTime.now());
-
-
             try {
                 transactionService.saveTransaction(body);
             } catch (Exception e) {
@@ -141,9 +84,8 @@ public class TransactionsApiController implements TransactionsApi {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
             }
             System.out.println("print the body");
-            //System.out.println(body);
 
-            return new ResponseEntity<Transaction>(HttpStatus.OK);
+            return new ResponseEntity<Transaction>(HttpStatus.CREATED);
         } else {
             return new ResponseEntity(HttpStatus.FORBIDDEN);
         }
